b4872514fd57528e7202acff7bf549350838648a
Karan Jadhav <jadhavkaran3636@gmail.com>
2020-11-20 18:04:43 +0530

    added

--- db.js
+++ db.js
@@ -7,11 +7,13 @@
 //   port: 5432,
 // });
 
-// module.exports = pool;
+
 const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
+
   }
 });
+module.exports = pool;
