86400cb4d6906122376d01ff28474331edcf83d1
Karan Jadhav <jadhavkaran3636@gmail.com>
2020-11-23 12:27:30 +0530

    added

--- database.sql
+++
@@ -1,8 +0,0 @@
--- CREATE DATABASE task_database;
--- --\c into database
-
--- CREATE TABLE task(
---     task_id SERIAL PRIMARY KEY,
---     task_name VARCHAR(255),
---     task_date VARCHAR(255)
--- );
