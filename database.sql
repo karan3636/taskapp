7a28a311c3a3e8771ad5c1c34b8a3cb1078b3ef3
Karan Jadhav <jadhavkaran3636@gmail.com>
2020-11-23 12:25:32 +0530

    added

--- database.sql
+++ database.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE task_database;
---\c into database
+-- CREATE DATABASE task_database;
+-- --\c into database
 
-CREATE TABLE task(
-    task_id SERIAL PRIMARY KEY,
-    task_name VARCHAR(255),
-    task_date VARCHAR(255)
-);
+-- CREATE TABLE task(
+--     task_id SERIAL PRIMARY KEY,
+--     task_name VARCHAR(255),
+--     task_date VARCHAR(255)
+-- );
